787c8e6e80eca806fa0ff7680e9dd02a2c741541
Nariman <abonent26pg@mail.ru>
2023-03-07 22:21:52 +0300

    init

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Veggie from "../components/Veggie";
+import Popular from "../components/Popular";
+const Home = () => {
+  return (
+    <>
+      <Veggie />
+      <Popular />
+    </>
+  );
+};
+
+export default Home;
