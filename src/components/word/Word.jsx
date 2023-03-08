e81cda079093eac34c35dfe708683b16cac1d2f6
Nariman <abonent26pg@mail.ru>
2023-03-08 14:08:28 +0300

    added audiplayer

--- src/components/word/Word.jsx
+++ src/components/word/Word.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styles from "../../styles/word.module.scss";
 import { BsFillPlayFill } from "react-icons/bs";
 import { useSelector } from "react-redux";
 const Word = () => {
-  const { word, trans, error } = useSelector(({ word }) => word);
+  const { word, trans, error, audio } = useSelector(({ word }) => word);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioElem = useRef();
+
+  const PlayPause = () => {
+    audioElem.current.play();
+  };
 
   return (
     <div className={styles.word}>
+      <audio src={audio} ref={audioElem} />
       <div className={styles.inner}>
         <h1>{error ? "sorry we don`t know this word" : word}</h1>
-        <div className={styles.playIcon}>
-          <BsFillPlayFill color="#A647ED" />
-        </div>
+        {audio ? (
+          <button className={styles.playIcon} onClick={PlayPause}>
+            <BsFillPlayFill color="#A647ED" />
+          </button>
+        ) : null}
       </div>
       <h3 className={styles.desc}>{trans}</h3>
     </div>
